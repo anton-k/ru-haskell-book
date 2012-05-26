f1cab35082c675eeda9baa1c869c4197cbc90091
Dmitry Malikov <color404051h@gmail.com>
2012-05-27 02:27:42 +0400

    Tex: correct sh-scripts

--- tex/notes.sh
+++ tex/notes.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e # abort script when a command exits with non-zero status
+
 # important flag "-8bit" to cure tab displaying bug in xelatex
 xelatex --shell-escape notes.tex
 #rm *.aux
