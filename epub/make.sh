7f61be229dd80b2adc6109bd447d72e6602e8894
Anton Kholomiov <anton.kholomiov@gmail.com>
2012-08-14 00:13:43 +0400

    epub spec

---
+++ epub/make.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# set -e # abort script when a command exits with non-zero status
+
+pandoc -S --toc \
+    --epub-stylesheet=epub-style.css \
+    --epub-metadata=metadata.xml \
+    -o ru-haskell-book.epub \
+        title.txt \
+        ../pandoc/preface.txt \
+        ../pandoc/1.txt \
+        ../pandoc/2.txt \
+        ../pandoc/3.txt \
+        ../pandoc/4.txt \
+        ../pandoc/5.txt \
+        ../pandoc/6.txt \
+        ../pandoc/7.txt \
+        ../pandoc/8.txt \
+        ../pandoc/9.txt \
+        ../pandoc/10.txt \
+        ../pandoc/11.txt \
+        ../pandoc/12.txt \
+        ../pandoc/13.txt \
+        ../pandoc/14.txt \
+        ../pandoc/15.txt \
+        ../pandoc/16.txt \
+        ../pandoc/17.txt \
+        ../pandoc/18.txt \
+        ../pandoc/19.txt \
+        ../pandoc/20.txt \
+        ../pandoc/21.txt \
+        ../pandoc/appendix.txt
+
